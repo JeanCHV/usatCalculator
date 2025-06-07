3c01631522f6cbc62a18818bd973d99d781bc462
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2025-06-06 23:13:42 -0500

    fix: service worker corregido

--- service-worker.js
+++ service-worker.js
@@ -3,18 +3,8 @@ self.addEventListener('install', event => {
     caches.open('usat-calc-v1').then(cache => {
       return cache.addAll([
         '/',
-        'index.html',
-        'css/style.css',
-        'js/app.js',
-        'js/utils.js',
-        'js/db.js',
         'assets/img/usat_logo.png',
         'assets/img/usat_logo_red.jpg',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
-        'https://code.jquery.com/jquery-3.7.1.min.js',
-        'https://cdn.jsdelivr.net/npm/sweetalert2@11',
-        'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js',
       ]);
     })
   );
